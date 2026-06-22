1b8e85361f03ed9cd9e8b4bd780e69977a81d5ad
shruti patidar <shrutimohobiya@gmail.com>
2026-06-22 23:24:26 +0530

    Update retail db.sql

--- retail db.sql
+++ retail db.sql
@@ -1,94 +1,160 @@
-select * from orders
+-- =====================================================
+-- Retail Sales Analysis
+-- =====================================================
 
---find top 10 highest reveue generating products 
-select product_id,sum(quantity*sale_price) as total_revenue
-from orders
-group by product_id
-order by total_revenue desc
-limit 10
+-- Objective:
+-- Analyze retail sales data to uncover insights related to
+-- revenue, profitability, product performance, regional trends,
+-- and year-over-year growth.
 
---find top 5 highest selling products in each region
-with cte as (
-select region,product_id,sum(sale_price) as sales
-from orders
-group by region,product_id)
-select * from (
-select *, row_number() over(partition by region order by sales desc) as rn
-from cte) A
-where rn<=5
+-- =====================================================
+-- Business Problem 1
+-- Top 10 Revenue Generating Products
+-- =====================================================
 
+-- Business Question:
+-- Which products generate the highest revenue for the business?
 
+SELECT
+product_id,
+SUM(quantity * sale_price) AS total_revenue
+FROM orders
+GROUP BY product_id
+ORDER BY total_revenue DESC
+LIMIT 10;
 
---find month over month growth comparison for 2022 and 2023 sales eg : jan 2022 vs jan 2023
+-- Insight:
+-- TEC-CO-10004722 was the highest revenue-generating product.
+
+-- =====================================================
+-- Business Problem 2
+-- Top 5 Revenue Generating Products by Region
+-- =====================================================
+
+-- Business Question:
+-- Which products generate the highest revenue within each region?
+
 WITH cte AS (
-    SELECT
-        EXTRACT(YEAR FROM order_date) AS order_year,
-        EXTRACT(MONTH FROM order_date) AS order_month,
-        SUM(sale_price) AS sales
-    FROM orders
-    GROUP BY
-        EXTRACT(YEAR FROM order_date),
-        EXTRACT(MONTH FROM order_date)
+SELECT
+region,
+product_id,
+SUM(quantity * sale_price) AS revenue
+FROM orders
+GROUP BY region, product_id
+)
+SELECT *
+FROM (
+SELECT *,
+ROW_NUMBER() OVER (
+PARTITION BY region
+ORDER BY revenue DESC
+) AS rn
+FROM cte
+) a
+WHERE rn <= 5;
+
+-- Insight:
+-- Refer to output for top-performing products across regions.
+
+-- =====================================================
+-- Business Problem 3
+-- Year-over-Year Revenue Comparison (2022 vs 2023)
+-- =====================================================
+
+-- Business Question:
+-- How did monthly revenue performance change from 2022 to 2023?
+
+WITH cte AS (
+SELECT
+EXTRACT(YEAR FROM order_date) AS order_year,
+EXTRACT(MONTH FROM order_date) AS order_month,
+SUM(quantity * sale_price) AS revenue
+FROM orders
+GROUP BY
+EXTRACT(YEAR FROM order_date),
+EXTRACT(MONTH FROM order_date)
 )
 SELECT
-    order_month,
-    SUM(CASE WHEN order_year = 2022 THEN sales ELSE 0 END) AS sales_2022,
-    SUM(CASE WHEN order_year = 2023 THEN sales ELSE 0 END) AS sales_2023
+order_month,
+SUM(CASE WHEN order_year = 2022 THEN revenue ELSE 0 END) AS revenue_2022,
+SUM(CASE WHEN order_year = 2023 THEN revenue ELSE 0 END) AS revenue_2023
 FROM cte
 GROUP BY order_month
 ORDER BY order_month;
 
+-- Insight:
+-- Refer to output for monthly revenue comparison between 2022 and 2023.
 
+-- =====================================================
+-- Business Problem 4
+-- Highest Revenue Month by Category
+-- =====================================================
 
+-- Business Question:
+-- Which month generated the highest revenue for each category?
 
-
---for each category which month had highest sales 
 WITH cte AS (
-    SELECT
-        category,
-        TO_CHAR(order_date, 'YYYY-MM') AS order_year_month,
-        SUM(sale_price) AS sales
-    FROM orders
-    GROUP BY
-        category,
-        TO_CHAR(order_date, 'YYYY-MM'))
 SELECT
-    category,
-    order_year_month,
-    sales
-FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY category ORDER BY sales DESC) AS rn FROM cte) a
+category,
+TO_CHAR(order_date, 'YYYY-MM') AS order_year_month,
+SUM(quantity * sale_price) AS revenue
+FROM orders
+GROUP BY
+category,
+TO_CHAR(order_date, 'YYYY-MM')
+)
+SELECT
+category,
+order_year_month,
+revenue
+FROM (
+SELECT *,
+ROW_NUMBER() OVER (
+PARTITION BY category
+ORDER BY revenue DESC
+) AS rn
+FROM cte
+) a
 WHERE rn = 1;
 
+-- Insight:
+-- Identified the highest revenue-generating month for each category.
 
+-- =====================================================
+-- Business Problem 5
+-- Highest Profit Growth Sub-Category
+-- =====================================================
 
+-- Business Question:
+-- Which sub-category experienced the highest profit growth
+-- in 2023 compared to 2022?
 
-
-
---which sub category had highest growth by profit in 2023 compare to 2022
 WITH cte AS (
-    SELECT
-        sub_category,
-        EXTRACT(YEAR FROM order_date) AS order_year,
-        SUM(profit) AS profit
-    FROM orders
-    GROUP BY
-        sub_category,
-        EXTRACT(YEAR FROM order_date)
+SELECT
+sub_category,
+EXTRACT(YEAR FROM order_date) AS order_year,
+SUM(profit) AS profit
+FROM orders
+GROUP BY
+sub_category,
+EXTRACT(YEAR FROM order_date)
 ),
 cte2 AS (
-    SELECT
-        sub_category,
-        SUM(CASE WHEN order_year = 2022 THEN profit ELSE 0 END) AS profit_2022,
-        SUM(CASE WHEN order_year = 2023 THEN profit ELSE 0 END) AS profit_2023
-    FROM cte
-    GROUP BY sub_category
-)
-
 SELECT
-    sub_category,
-    profit_2022,
-    profit_2023,
-    (profit_2023 - profit_2022) AS growth
+sub_category,
+SUM(CASE WHEN order_year = 2022 THEN profit ELSE 0 END) AS profit_2022,
+SUM(CASE WHEN order_year = 2023 THEN profit ELSE 0 END) AS profit_2023
+FROM cte
+GROUP BY sub_category
+)
+SELECT
+sub_category,
+profit_2022,
+profit_2023,
+(profit_2023 - profit_2022) AS growth
 FROM cte2
 ORDER BY growth DESC
 LIMIT 1;
+
+-- Insight:
+-- Identified the sub-category with the highest year-over-year profit growth.
